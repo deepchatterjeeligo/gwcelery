47be489669674be7a14a57d0ee75b2df770586f7
bence.becsy <bence.becsy@ligo.org>
2019-02-17 19:05:40 -0800

    Initial commit

---
+++ gwcelery/tasks/bayeswave.py
@@ -0,0 +1,45 @@
+"""Follow-up analysis with BayesWave."""
+from distutils.spawn import find_executable
+from distutils.dir_util import mkpath
+import glob
+import json
+import math
+import os
+import shutil
+import subprocess
+import tempfile
+
+from celery import group
+from glue.lal import Cache
+from gwdatafind import find_urls
+from gwpy.timeseries import StateVector
+import lal
+import lalsimulation
+
+from .. import app
+from ..jinja import env
+from . import condor
+from . import gracedb
+
+
+ini_name = '/home/bence.becsy/O3/zero_lag/zero_lag.ini'
+pipepath = '/home/bence.becsy/O3/BW/bin/bayeswave_pipe'
+
+
+@app.task(ignore_result=True, shared=False)
+def start_bayeswave(ini_contents, preferred_event_id, superevent_id):
+    """Run BayesWave on a given event.
+
+    Parameters
+    ----------
+    ini_contents : str
+        The content of online_pe.ini
+    preferred_event_id : str
+        The GraceDb ID of a target preferred event
+    superevent_id : str
+        The GraceDb ID of a target superevent
+    """
+    # make a run directory
+    workdir = '/home/bence.becsy/O3/zero_lag/jobs/'+graceid
+
+    print(workdir)
