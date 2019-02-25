fb6fd4bec6aada4dd5b6967efb7354a73a869d10
bence.becsy <bence.becsy@ligo.org>
2019-02-25 10:43:13 -0800

    Further tweaks to run with system-wide BayesWave and bayeswave_pipe

--- gwcelery/tasks/bayeswave.py
+++ gwcelery/tasks/bayeswave.py
@@ -25,9 +25,10 @@ from . import gracedb
 
 ini_name = '/home/bence.becsy/O3/zero_lag/zero_lag.ini'
 #path to conda system-wide release of BayesWave
-bw_prefix = '/cvmfs/ligo-containers.opensciencegrid.org/lscsoft/conda/latest/envs/ligo-py37/bin/'
-pipepath = '/home/bence.becsy/O3/BW/bin/bayeswave_pipe'
-
+#bw_prefix = '/cvmfs/ligo-containers.opensciencegrid.org/lscsoft/conda/latest/envs/ligo-py37/bin/'
+bw_prefix = '/cvmfs/ligo-containers.opensciencegrid.org/lscsoft/conda/latest/envs/ligo-py27/bin/'
+#pipepath = '/home/bence.becsy/O3/BW/bin/bayeswave_pipe'
+pipepath = bw_prefix + 'bayeswave_pipe'
 
 @app.task(ignore_result=True, shared=False)
 def start_bayeswave(preferred_event_id, superevent_id):
@@ -51,7 +52,8 @@ def start_bayeswave(preferred_event_id, superevent_id):
     
     # -- Set up call to pipeline -- Niter=1000 for very quick tests
     #Added "python2.7" before the call to force it to use python 2.7
-    pipe_call = 'export PYTHONPATH={extra_path}:${{PYTHONPATH}}; python2.7 {pipepath} {inifile} \
+    #pipe_call = 'export PYTHONPATH={extra_path}:${{PYTHONPATH}}; python2.7 {pipepath} {inifile} \
+    pipe_call = 'python2.7 {pipepath} {inifile} \
     --workdir {workdir} \
     --graceID {graceid} \
     --condor-submit'.format(extra_path=pypath_to_add ,pipepath=pipepath, inifile=ini_name, workdir=workdir, graceid=preferred_event_id)
