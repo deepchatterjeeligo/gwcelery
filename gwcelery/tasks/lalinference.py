924b4a740c5646b19ac511551bf0b77614f8b4f6
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-08-22 11:59:50 -0700

    upload lalinference result file

--- gwcelery/tasks/lalinference.py
+++ gwcelery/tasks/lalinference.py
@@ -416,9 +416,9 @@ def dag_finished(rundir, preferred_event_id, superevent_id):
         _upload_url.si(pe_results_path, superevent_id) |
         group(
             _upload_result(
-                pe_results_path, 'posterior_samples.json', superevent_id,
+                rundir, 'posterior*.hdf5', superevent_id,
                 'LALInference posterior samples', 'pe',
-                'LALInference.posterior_samples.json'
+                'LALInference.posterior_samples.hdf5'
             ),
             _upload_result(
                 pe_results_path, 'Online_extrinsic.png', superevent_id,
